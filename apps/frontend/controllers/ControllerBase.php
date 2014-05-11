274f22c11090285c7c2113ce555ccd7f7cd75576
Simon Jonsson <nohatssir@users.noreply.github.com>
2014-05-11 16:08:04 +0200

    Add forward function to frontend basecontroler

--- apps/frontend/controllers/ControllerBase.php
+++ apps/frontend/controllers/ControllerBase.php
@@ -4,5 +4,17 @@ namespace Pangea\Frontend\Controllers;
 
 class ControllerBase extends \Phalcon\Mvc\Controller
 {
+    protected function forward($uri)
+    {
+        $uriParts = explode("/", $uri);
 
+        var_dump($uriParts);
+
+        $this->dispatcher->forward(
+            array(
+                "controller" => $uriParts[0],
+                "action" => $uriParts[1]
+            )
+        );
+    }
 }
