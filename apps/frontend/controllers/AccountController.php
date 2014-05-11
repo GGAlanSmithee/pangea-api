3f8e0653e687d631557f68766da4ce641ed36ce0
Simon Jonsson <nohatssir@users.noreply.github.com>
2014-05-11 16:34:40 +0200

    Add functions to AccountController

--- apps/frontend/controllers/AccountController.php
+++ apps/frontend/controllers/AccountController.php
@@ -4,6 +4,87 @@ namespace Pangea\Frontend\Controllers;
 
 class AccountController extends ControllerBase
 {
-    public function indexAction() {}
-}
+    /**
+     * Register a session to mark user as logged in
+     *
+     * @param string user User to register
+     */
+    private function registerSession($user)
+    {
+        $this->session->set("auth", array(
+            "id" => $user->getId(),
+            "username" => $user->getUsername(),
+        ));
+    }
 
+    /**
+     * Authenticate a user with username and password by POST
+     */
+    public function loginAction()
+    {
+        if ($this->request->isPost()) {
+            $username = $this->request->getPost("username");
+            $password = $this->request->getPost("password");
+
+            $user = User::findByFirstUsername($username);
+
+            if ($user)
+            {
+                if ($this->security->checkHash($password, $user->getPassword()))
+                {
+                    $this->registerSession($user);
+                    $this->forward("index/index");
+                }
+            }
+
+            // Notify about wrong username/password
+            $this->flash->error("Wrong username/password");
+        }
+
+        $this->forward("index/index");
+    }
+
+    /**
+     * Remove the current session that marks a
+     * user as authenticated
+     */
+    public function logoutAction()
+    {
+        $this->session->remove("auth");
+        $this->forward("index/index");
+    }
+
+    /**
+     * Register a new user
+     */
+    public function registerAction()
+    {
+        if ($this->request->isPost()) {
+            $username = $this->request->getPost("username");
+            $password = $this->request->getPost("password");
+            $firstName = $this->request->getPost("first_name");
+            $lastName = $this->request->getPost("last_name");
+            $email = $this->request->getPost("email");
+
+            $user = User::findByFirstUsername($username);
+
+            if ($user)
+            {
+                // Notify that the username is taken
+                $this->flash->error("Username is taken");
+            }
+            else
+            {
+                $user = new User();
+                $user->setUsername($username);
+                $user->setPassword($this->security->hash($password));
+                $user->setFirstName($firstName);
+                $user->setLastName($lastName);
+                $user->setEmail($email);
+                $user->save();
+            }
+
+            $this->forward("index/index");
+        }
+    }
+}
