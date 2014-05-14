3cac1ecd6ca49b535e726c79e35c44efd54d8e0c
Simon Jonsson <nohatssir@users.noreply.github.com>
2014-05-14 23:40:33 +0200

    Change single qoutes to double in routes

--- routes/RaceRoutes.php
+++ routes/RaceRoutes.php
@@ -10,21 +10,21 @@ class RaceRoutes extends \Phalcon\Mvc\Router\Group
     {
         // Default paths
         $this->setPaths(array(
-            'module' => 'api',
-            'controller' => 'race',
-            'namespace' => 'Pangea\Api\Controllers'
+            "module" => "api",
+            "controller" => "race",
+            "namespace" => "Pangea\Api\Controllers"
         ));
 
         // All the routes start with /races
-        $this->setPrefix('/api/races');
+        $this->setPrefix("/api/races");
 
         // Routes
-        $this->add('', array('action' => 'index'));
+        $this->add("", array("action" => "index"));
 
         // CRUD
-        $this->addGet   ('/:int', array('action' => 'details', 'id' => 1));
-        //$this->addDelete('/:int', array('action' => 'delete', 'id' => 1));
-        //$this->addPut   ('/:int', array('action' => 'update', 'id' => 1));
-        //$this->addPost  ('', array('action' => 'create'));
+        $this->addGet   ("/:int", array("action" => "details", "id" => 1));
+        //$this->addDelete("/:int", array("action" => "delete", "id" => 1));
+        //$this->addPut   ("/:int", array("action" => "update", "id" => 1));
+        //$this->addPost  ("", array("action" => "create"));
     }
 }
